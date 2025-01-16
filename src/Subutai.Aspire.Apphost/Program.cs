53c05817b1147f76529c0bc76dd58976755b68e4
BruteDeepForce <rock.fear@windowslive.com>
2025-01-16 19:16:04 +0300

    aspire connect and postgresql

--- src/Subutai.Aspire.Apphost/Program.cs
+++ src/Subutai.Aspire.Apphost/Program.cs
@@ -1,8 +1,30 @@
+using Projects;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Aspire;
+using Google.Protobuf.WellKnownTypes;
+
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = DistributedApplication.CreateBuilder(args);
+
+        // var postgres = builder.AddPostgres("postgres");
+        // var postgresdb = postgres.AddDatabase("postgresdb");
+
+        // builder.AddProject<Projects.Subutai_WebApi>("webapi").WithReference(postgresdb);
+
+        var postgres = builder.AddPostgres("postgres")
+                      .WithPgAdmin();
+
+        var postgresdb = postgres.AddDatabase("postgresdb");
+
+        var exampleProject = builder.AddProject<Projects.Subutai_WebApi>("webapi")
+                            .WithReference(postgresdb);
+
 
         builder.Build().Run();
     }
--- src/Subutai.WebApi/Program.cs
+++ src/Subutai.WebApi/Program.cs
@@ -3,9 +3,9 @@ using Subutai.Repository.SqlRepository.Contexts;
 using Subutai.Repository.SqlRepository.Repositories;
 using Subutai.Service;
 using Microsoft.EntityFrameworkCore;
-
-
-
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Aspire.Npgsql.EntityFrameworkCore.PostgreSQL;
+ 
 namespace Subutai.WebApi;
 
 public class Program
@@ -16,15 +16,17 @@ public class Program
 
         // Add services to the container.
         builder.AddServiceDefaults();
+
         
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
-        // // PostgreSQL veritabanı bağlantısını yapılandırma
-        // builder.Services.AddDbContext<SubutaiContext>(options =>
-        // options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), x=> x.MigrationsAssembly("Subutai.Repository.SqlRepository")));   
+        // // PostgreSQL Local veritabanı bağlantısını yapılandırma ayarları
+        //builder.Services.AddDbContext<SubutaiContext>(options =>
+        //options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), x=> x.MigrationsAssembly("Subutai.Repository.SqlRepository")));   
 
+        builder.AddNpgsqlDbContext<SubutaiContext>("postgresdb");
         builder.Services.AddServiceDependencies();
         builder.Services.AddScoped<ISubutaiContext, SubutaiContext>();
         builder.Services.AddScoped<IProjectEntityRepository, ProjectEntityRepository>();
