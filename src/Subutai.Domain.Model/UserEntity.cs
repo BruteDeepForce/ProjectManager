2bd1cc8347210a5575f9dd26bf81eee8da9dd087
BruteDeepForce <rock.fear@windowslive.com>
2025-01-19 19:04:39 +0300

    UserEntity datetime

--- src/Subutai.Domain.Model/UserEntity.cs
+++ src/Subutai.Domain.Model/UserEntity.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Subutai.Domain.Model.Auditing;
 
 namespace Subutai.Domain.Model
 {
-    public class UserEntity
+    public class UserEntity : IHasCreationTime, IHasDeletionTime, IHasModificationTime
     {       
         [Key]
         public int Id { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; } 
         public string? Email { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DeletedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
